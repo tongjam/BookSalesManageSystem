2136e90a0cd54e7bf39bd3ff9f6db72b50d8c662
WangTong <1953373104@qq.com>
2020-12-26 02:30:03 +0800

    Init

---
+++ MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//download by http://down.liehuo.net
+namespace 书店零售系统
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            BookForm myBookForm = new BookForm();
+            myBookForm.Show();
+        }
+
+        private void btnHuiyuan_Click(object sender, EventArgs e)
+        {
+            CustomerForm myCusotmerForm = new CustomerForm();
+            myCusotmerForm.Show();
+        }
+
+        private void btnShouyin_Click(object sender, EventArgs e)
+        {
+            SellForm mySellForm = new SellForm();//5~1+a+s+p+x
+            mySellForm.Show();
+        }
+
+        private void btnTongji_Click(object sender, EventArgs e)
+        {
+            ReportForm myReportForm = new ReportForm();
+            myReportForm.Show();
+        }
+    }
+}
